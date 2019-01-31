3db77bf5683bffd3d07a749d4f8e5117613ada4e
AlexLzy <niceguylzy@gmail.com>
2019-01-31 13:55:19 +0800

    Complete repeatch method and beautify the code
    Preliminarily complete the repeatch method with a log mode and a verbose
    mode (but not add a __doc__ to the method)
    Add comments
    Rewritten a comment to make the code looks better

--- Clsc.py
+++ Clsc.py
@@ -38,7 +38,33 @@ class Clsc():
         f = re.sub('[({[].*?一作.*?[)}]', '', f)
         parsedfile = re.sub(rule, ',', f).strip().split(',')[:-1]
         title = parsedfile[0]
-        author = parsedfile[1]     #这里有可能会出现作者和朝代的解析问题
+        author = parsedfile[1]     # 这里有可能会出现作者和朝代的解析问题
         return (parsedfile, title, author, var)
 
+    def repeatch(self, arg=''):
+        # TODO: 添加__doc__
+        # TODO: 可以试着将作者和朝代都直接排除掉
+        # XXX:
+        # 这里排除冗余结果是用在内部直接排除作者和朝代还是
+        # 通过禁用字功能排除还有待商榷。
+        try:
+            if '-l' in arg:
+                sys.stdout = open('repeatch.txt', 'w')
+                # 将结果写入文件,文件名为repeatch.txt且只能在代码内部更改。
+            for e in set(''.join(self.parsed()[0])):  # e是古文中的每'一个字'
+                if ''.join(self.parsed()[0]).count(e) == 1:
+                    continue  # 如果只出现一次就跳过
+                if '-v' in arg:  # 唠叨模式，其实可以用来做归纳
+                    sys.stdout.write("重复的字："+c+"\n==================\n")
+                    for s in self.parsed()[0]:  # s指古文中的每一个短句
+                        if e in s:
+                            sys.stdout.write(s+"\n")
+                    sys.stdout.write("\n")
+                else:
+                    sys.stdout.write(e+"\n")
+        finally:
+            sys.stdout = self.__terminal__
 
+
+c = Clsc()
+c.repeatch()
